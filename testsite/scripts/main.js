0a1a7c7e6e2166d8224354edecc77f0dff8b640a
karlvaba <karl.vaba@gmail.com>
2017-09-06 13:18:15 +0300

    Add files via upload

---
+++ testsite/scripts/main.js
@@ -0,0 +1,34 @@
+
+
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+var myButton = document.querySelector('button');
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/af.jpg') {
+      myImage.setAttribute ('src','images/af2.jpg');
+    } else {
+      myImage.setAttribute ('src','images/af.jpg');
+    }
+}
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
